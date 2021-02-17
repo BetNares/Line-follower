c2274d55f2db655c94365ca3855385a9beb12921
BetNares <72925939+BetNares@users.noreply.github.com>
2021-02-17 16:31:15 +0700

    nyamain sama punya evan

--- shit i dont knwo/js/countdown.js
+++ shit i dont knwo/js/countdown.js
@@ -2,15 +2,15 @@ let timer;
 let minute = 4;
 let seconds = minute * 60
 let textSec = "00"
-let textMilsec = "000"
 let display = document.getElementById("display")
 display.innerHTML = minute + ':00'
+let statSec = 60
 
 function start() {
-    let statSec = 60
-    cancel()
+    if (seconds != 240) stop()
     timer = setInterval(function() {
-        seconds--;
+        seconds--; 
+        temp = seconds
         if (statSec != 0) statSec--
         else statSec = 59
         
@@ -18,7 +18,7 @@ function start() {
         else textSec = statSec
         
         display.innerHTML = Math.floor(seconds/60) + ':' + textSec
-
+        temp2 = statSec
         if (seconds == 0) {
             clearInterval(timer)
         }
@@ -27,10 +27,14 @@ function start() {
 
 function stop() {
     clearInterval(timer)
+    seconds = temp
+    statSec = temp2
 }
 
 function cancel() {
     clearInterval(timer)
     display.innerHTML = minute + ':00'
+    minute = 4
     seconds = minute * 60
+    statSec = 60
 }
